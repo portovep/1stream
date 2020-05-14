dbe81e57f666b428ee8045a1ed68401c2463b6c8
Pablo Porto <portovep@tcd.ie>
2020-05-14 08:52:56 +0200

    Integrate networking logic with Chrome extension for Netflix

--- chrome_extension_poc/background.js
+++ chrome_extension_poc/background.js
@@ -1,27 +1,58 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
-    });
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([{
-          conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostEquals: 'www.netflix.com'},
-          })
-          ],
-              actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-    });
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: "www.netflix.com" },
+          }),
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()],
+      },
+    ]);
+  });
 });
 
-chrome.runtime.onConnect.addListener(function(port) {
-    console.assert(port.name == "knockknock");
-    port.onMessage.addListener(function(msg) {
-      if (msg.joke == "Knock knock")
-        port.postMessage({question: "Who's there?"});
-      else if (msg.answer == "Madame")
-        port.postMessage({question: "Madame who?"});
-      else if (msg.answer == "Madame... Bovary")
-        port.postMessage({question: "I don't get it."});
+chrome.webNavigation.onCompleted.addListener(
+  function(event) {
+    chrome.tabs.sendMessage(event.tabId, { command: "CONNECT" }, function(
+      response
+    ) {
+      console.log(
+        "Tab with ID " + event + " responded with status: " + response.status
+      );
     });
-  });
+  },
+  {
+    url: [
+      {
+        urlMatches: "https://www.netflix.com/watch/*",
+        queryContains: "roomName",
+      },
+    ],
+  }
+);
 
+chrome.pageAction.onClicked.addListener(function(tab) {
+  console.log("Page Action on click fired for tab ID: " + tab.id);
+  console.log("and URL: " + tab.url);
+
+  console.log("Sending start command to tab ID: " + tab.id);
+
+  chrome.tabs.sendMessage(tab.id, { command: "START" }, function(response) {
+    console.log(
+      "Tab with ID " + tab.id + " responded with status: " + response.status
+    );
+  });
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.status) {
+    console.log(
+      "Tab with ID " +
+        sender.tab.id +
+        " responded with status: " +
+        request.status
+    );
+  }
+});
--- chrome_extension_poc/contentScript.js
+++ chrome_extension_poc/contentScript.js
@@ -1,269 +1,71 @@
-(function() {
-  // Define "global" variables
+console.log("Waiting for START command from extension");
 
-  var servers = {
-    iceServers: [
-      { urls: "stun:stun.services.mozilla.com" },
-      { urls: "stun:stun.l.google.com:19302" },
-    ],
-  };
+function startup() {
+  var roomName = generateRoomName();
+  connectToSignalingServer(roomName);
 
-  var connectButton = null;
-  var disconnectButton = null;
-  var sendButton = null;
-  var messageInputBox = null;
-  var receiveBox = null;
+  var video = document.getElementsByTagName("video")[0];
+  console.log("got video element", video);
 
-  var leftVideo = null;
-  var rightVideo = null;
+  video.pause();
 
-  var localConnection = null; // RTCPeerConnection for our "local" connection
-  var remoteConnection = null; // RTCPeerConnection for the "remote"
+  console.log("configuring listeners");
+  bindEventListeners(video);
 
-  var sendChannel = null; // RTCDataChannel for the local (sender)
-  var receiveChannel = null; // RTCDataChannel for the remote (receiver)
+  printURLToShare(roomName);
 
-  // Functions
+  reportStatusToContentScript("STARTED");
+}
 
-  // Set things up, connect event listeners, etc.
+function connect() {
+  const roomName = extractRoomNameFromURL();
+  console.log("Connecting to room name: " + roomName);
 
-  function startup() {
-    connectButton = document.getElementById("connectButton");
-    leftVideo = document.getElementById("leftVideo");
-    rightVideo = document.getElementById("rightVideo");
-    disconnectButton = document.getElementById("disconnectButton");
-    sendButton = document.getElementById("sendButton");
-    messageInputBox = document.getElementById("message");
-    receiveBox = document.getElementById("receivebox");
+  var video = document.getElementsByTagName("video")[0];
+  setVideo(video);
 
-    // Set event listeners for user interface widgets
+  connectToSignalingServer(roomName);
+}
 
-    connectButton.addEventListener("click", connectPeers, false);
-    disconnectButton.addEventListener("click", disconnectPeers, false);
-    sendButton.addEventListener("click", sendMessage, false);
-
-    leftVideo.addEventListener("play", (event) => {
-      const currentTime = parseFloat(leftVideo.currentTime.toFixed(2));
-      console.log("Play" + currentTime);
-      sendChannel.send(
-        JSON.stringify({
-          type: "PLAY",
-          currentTime: currentTime,
-        })
-      );
-    });
-
-    leftVideo.addEventListener("pause", (event) => {
-      const currentTime = parseFloat(leftVideo.currentTime.toFixed(2));
-      console.log("Pause" + currentTime);
-      sendChannel.send(
-        JSON.stringify({
-          type: "PAUSE",
-          currentTime: currentTime,
-        })
-      );
-    });
-
-    // leftVideo.ontimeupdate = (event) => {
-    //   console.log("The currentTime attribute has been updated. Again.");
-    //   console.log(event);
-    // };
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  console.log("Received command " + request.command + " from extension");
+  if (request.command == "START") {
+    console.log("Reporting status STARTING to extension");
+    sendResponse({ status: "STARTING" });
+    startup();
+  } else if (request.command == "CONNECT") {
+    console.log("Reporting status CONNECTING to extension");
+    sendResponse({ status: "CONNECTING" });
+    connect();
   }
+});
 
-  // Connect the two peers. Normally you look for and connect to a remote
-  // machine here, but we're just connecting two local objects, so we can
-  // bypass that step.
+function reportStatusToContentScript(status) {
+  console.log("Reporting status " + status + " to extension");
+  chrome.runtime.sendMessage({ status });
+}
 
-  function listenToPeers() {
-    // Create the remote connection and its event listeners
-
-    remoteConnection = new RTCPeerConnection(servers);
-    remoteConnection.ondatachannel = receiveChannelCallback;
-
-    remoteConnection.onicecandidate = (e) =>
-      !e.candidate ||
-      localConnection
-        .addIceCandidate(e.candidate)
-        .catch(handleAddCandidateError);
+function extractRoomNameFromURL() {
+  const urlParams = new URLSearchParams(document.location.search);
+  const roomName = urlParams.get("roomName");
+  if (!roomName) {
+    throw "No roomName was found in the URL";
   }
+  return roomName;
+}
 
-  function connectPeers() {
-    // Create the local connection and its event listeners
+function generateRoomName() {
+  return uuidv4();
+}
 
-    localConnection = new RTCPeerConnection(servers);
+function printURLToShare(roomName) {
+  console.log(window.location + "&roomName=" + roomName);
+}
 
-    // Create the data channel and establish its event listeners
-    sendChannel = localConnection.createDataChannel("sendChannel");
-    sendChannel.onopen = handleSendChannelStatusChange;
-    sendChannel.onclose = handleSendChannelStatusChange;
-
-    // Set up the ICE candidates for the two peers
-
-    localConnection.onicecandidate = (e) =>
-      !e.candidate ||
-      remoteConnection
-        .addIceCandidate(e.candidate)
-        .catch(handleAddCandidateError);
-
-    // Now create an offer to connect; this starts the process
-
-    localConnection
-      .createOffer()
-      .then((offer) => localConnection.setLocalDescription(offer))
-      .then(() =>
-        remoteConnection.setRemoteDescription(localConnection.localDescription)
-      )
-      .then(() => remoteConnection.createAnswer())
-      .then((answer) => remoteConnection.setLocalDescription(answer))
-      .then(() =>
-        localConnection.setRemoteDescription(remoteConnection.localDescription)
-      )
-      .catch(handleCreateDescriptionError);
-  }
-
-  // Handle errors attempting to create a description;
-  // this can happen both when creating an offer and when
-  // creating an answer. In this simple example, we handle
-  // both the same way.
-
-  function handleCreateDescriptionError(error) {
-    console.log("Unable to create an offer: " + error.toString());
-  }
-
-  // Handle successful addition of the ICE candidate
-  // on the "local" end of the connection.
-
-  function handleLocalAddCandidateSuccess() {
-    connectButton.disabled = true;
-  }
-
-  // Handle successful addition of the ICE candidate
-  // on the "remote" end of the connection.
-
-  function handleRemoteAddCandidateSuccess() {
-    disconnectButton.disabled = false;
-  }
-
-  // Handle an error that occurs during addition of ICE candidate.
-
-  function handleAddCandidateError() {
-    console.log("Oh noes! addICECandidate failed!");
-  }
-
-  // Handles clicks on the "Send" button by transmitting
-  // a message to the remote peer.
-
-  function sendMessage() {
-    var message = messageInputBox.value;
-    sendChannel.send(message);
-
-    // Clear the input box and re-focus it, so that we're
-    // ready for the next message.
-
-    messageInputBox.value = "";
-    messageInputBox.focus();
-  }
-
-  // Handle status changes on the local end of the data
-  // channel; this is the end doing the sending of data
-  // in this example.
-
-  function handleSendChannelStatusChange(event) {
-    if (sendChannel) {
-      var state = sendChannel.readyState;
-
-      if (state === "open") {
-        messageInputBox.disabled = false;
-        messageInputBox.focus();
-        sendButton.disabled = false;
-        disconnectButton.disabled = false;
-        connectButton.disabled = true;
-      } else {
-        messageInputBox.disabled = true;
-        sendButton.disabled = true;
-        connectButton.disabled = false;
-        disconnectButton.disabled = true;
-      }
-    }
-  }
-
-  // Called when the connection opens and the data
-  // channel is ready to be connected to the remote.
-
-  function receiveChannelCallback(event) {
-    receiveChannel = event.channel;
-    receiveChannel.onmessage = handleReceiveMessage;
-    receiveChannel.onopen = handleReceiveChannelStatusChange;
-    receiveChannel.onclose = handleReceiveChannelStatusChange;
-  }
-
-  // Handle onmessage events for the receiving channel.
-  // These are the data messages sent by the sending channel.
-
-  function handleReceiveMessage(event) {
-    var command = JSON.parse(event.data);
-    var el = document.createElement("p");
-    var text = command.type + " time: " + command.currentTime;
-    var txtNode = document.createTextNode(text);
-
-    el.appendChild(txtNode);
-    receiveBox.appendChild(el);
-
-    if (command.type === "PLAY") {
-      rightVideo.currentTime = command.currentTime;
-      rightVideo.play();
-      console.log("Playing right video at: " + command.currentTime);
-    } else if (command.type == "PAUSE") {
-      rightVideo.pause();
-      rightVideo.currentTime = command.currentTime;
-      console.log("Pausing right video at: " + command.currentTime);
-    }
-  }
-
-  // Handle status changes on the receiver's channel.
-
-  function handleReceiveChannelStatusChange(event) {
-    if (receiveChannel) {
-      console.log(
-        "Receive channel's status has changed to " + receiveChannel.readyState
-      );
-    }
-
-    // Here you would do stuff that needs to be done
-    // when the channel's status changes.
-  }
-
-  // Close the connection, including data channels if they're open.
-  // Also update the UI to reflect the disconnected status.
-
-  function disconnectPeers() {
-    // Close the RTCDataChannels if they're open.
-
-    sendChannel.close();
-    receiveChannel.close();
-
-    // Close the RTCPeerConnections
-
-    localConnection.close();
-    remoteConnection.close();
-
-    sendChannel = null;
-    receiveChannel = null;
-    localConnection = null;
-    remoteConnection = null;
-
-    // Update user interface elements
-
-    connectButton.disabled = false;
-    disconnectButton.disabled = true;
-    sendButton.disabled = true;
-
-    messageInputBox.value = "";
-    messageInputBox.disabled = true;
-  }
-
-  // Set up an event listener which will run the startup
-  // function once the page is done loading.
-
-  window.addEventListener("load", startup, false);
-})();
+function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
